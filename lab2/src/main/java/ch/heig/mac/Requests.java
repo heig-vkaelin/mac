0335a5df372f7ad570b90d358072356686fe62b2
Valentin Kaelin <vkaelin@hey.com>
2022-03-15 16:22:31 +0100

    feat: add queries 2 + 9

--- lab2/src/main/java/ch/heig/mac/Requests.java
+++ lab2/src/main/java/ch/heig/mac/Requests.java
@@ -7,55 +7,76 @@ import org.neo4j.driver.*;
 import org.neo4j.driver.Record;
 
 public class Requests {
-    private static final  Logger LOGGER = Logger.getLogger(Requests.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Requests.class.getName());
     private final Driver driver;
-
+    
     public Requests(Driver driver) {
         this.driver = driver;
     }
-
+    
     public List<String> getDbLabels() {
         var dbVisualizationQuery = "CALL db.labels";
-
+        
         try (var session = driver.session()) {
             var result = session.run(dbVisualizationQuery);
             return result.list(t -> t.get("label").asString());
         }
     }
-
+    
     public List<Record> possibleSpreaders() {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public List<Record> possibleSpreadCounts() {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        var query =
+                "MATCH(sick:Person{healthstatus:'Sick'})-[v1:VISITS]->(p:Place)<-[v2:VISITS]-" +
+                        "(healthy:Person{healthstatus:'Healthy'}) " +
+                        "WHERE v1.starttime > sick.confirmedtime AND " +
+                        "v2.starttime > sick.confirmedtime " +
+                        "RETURN sick.name AS sickName, COUNT(healthy) AS nbHealthy";
+        
+        try (var session = driver.session()) {
+            var result = session.run(query);
+            return result.list();
+        }
     }
-
+    
     public List<Record> carelessPeople() {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public List<Record> sociallyCareful() {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public List<Record> peopleToInform() {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public List<Record> setHighRisk() {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public List<Record> healthyCompanionsOf(String name) {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public Record topSickSite() {
         throw new UnsupportedOperationException("Not implemented, yet");
     }
-
+    
     public List<Record> sickFrom(List<String> names) {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        var query =
+                "MATCH (p:Person{healthstatus:'Sick'}) WHERE p.name IN " +
+                        "$names" +
+                        " RETURN p.name AS sickName";
+        
+        try (var session = driver.session()) {
+            var result = session.run(
+                    query,
+                    Values.parameters("names", names)
+            );
+            return result.list();
+        }
     }
 }
